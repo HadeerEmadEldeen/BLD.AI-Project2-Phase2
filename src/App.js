fe04ef978389a5cfec69a3b90247ac68dc0f424a
HadeerEmadEldeen <hadeeremadalden123@gmail.com>
2022-09-13 18:19:23 +0200

    for deploy

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import React from 'react';
 import './App.css';
 import NavBar  from './Component/NavBar';
 import {Routes , Route} from 'react-router-dom';
-import Home from './Component/Home';
 import Footer from './Component/Footer';
 import './Component/Style.css';
-import Routers from './Component/Routers';
 import FatchData from './Component/FatchData';
-import Page2 from './Component/page2/Page2';
 function App() {
   return (
     <div className='continer'>
