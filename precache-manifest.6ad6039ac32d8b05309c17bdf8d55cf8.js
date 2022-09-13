f886305c7b7638902bcde4a7ee2cd7105c6b75ea
HadeerEmadEldeen <hadeeremadalden123@gmail.com>
2022-09-13 18:20:44 +0200

    Updates

---
+++ precache-manifest.6ad6039ac32d8b05309c17bdf8d55cf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cada873829b6f6c6f779",
+    "url": "http//HadeerEmadEldeen.github.io/BLD.AI-Project2-Phase2/static/css/main.42a0e326.chunk.css"
+  },
+  {
+    "revision": "cada873829b6f6c6f779",
+    "url": "http//HadeerEmadEldeen.github.io/BLD.AI-Project2-Phase2/static/js/main.f965b3f6.chunk.js"
+  },
+  {
+    "revision": "950415ed1a7b08f5ab1d",
+    "url": "http//HadeerEmadEldeen.github.io/BLD.AI-Project2-Phase2/static/js/runtime~main.00533240.js"
+  },
+  {
+    "revision": "67f72fc88494e73c708c",
+    "url": "http//HadeerEmadEldeen.github.io/BLD.AI-Project2-Phase2/static/js/2.82035c0b.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "http//HadeerEmadEldeen.github.io/BLD.AI-Project2-Phase2/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "http//HadeerEmadEldeen.github.io/BLD.AI-Project2-Phase2/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8414f07b868ed5c1ca1fddd2201238c6",
+    "url": "http//HadeerEmadEldeen.github.io/BLD.AI-Project2-Phase2/index.html"
+  }
+];
